78a390d34d2f59707e3b32a687e5f0f8ef6fbee5
ryan <shariest@naver.com>
2025-03-19 15:29:40 +0900

    feature: #1 테이블 명 및 컬럼 명 수정

--- src/main/java/com/ryan/bookstore/entity/BooksEntity.java
+++ src/main/java/com/ryan/bookstore/entity/BooksEntity.java
@@ -17,35 +17,35 @@ import lombok.experimental.Accessors;
 @Data
 @Accessors(chain = true)
 @Entity
-@Table(name = "BOOKS")
+@Table(name = "books")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class BooksEntity {
 
     @Id
-    @Column(name = "BID", nullable = false, updatable = false)
+    @Column(name = "bid", nullable = false, updatable = false)
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Schema(description = "책 고유 ID", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
     private long bid;
 
     @NotNull
-    @Column(name = "TITLE", nullable = false)
+    @Column(name = "title", nullable = false)
     @Schema(description = "책 제목", example = "책 제목")
     private String title;
 
     @NotNull
-    @Column(name = "AUTHOR", nullable = false)
+    @Column(name = "author", nullable = false)
     @Schema(description = "저자", example = "저자")
     private String author;
 
     @NotNull
-    @Column(name = "PRICE", nullable = false)
+    @Column(name = "price", nullable = false)
     @Schema(description = "가격", example = "10000")
     private int price;
 
     @NotNull
-    @Column(name = "CNT", nullable = false)
+    @Column(name = "cnt", nullable = false)
     @Schema(description = "수량", example = "1")
     private int cnt;
 }
