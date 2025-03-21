e43b8b38085f84f4d4fac83df11da06e48560aef
Ryan <shariest@naver.com>
2025-03-21 15:14:40 +0900

    feature: #9 [BUG] No static resource 오류 수정 (#10)

--- src/main/java/com/ryan/bookstore/config/WebConfig.java
+++ src/main/java/com/ryan/bookstore/config/WebConfig.java
@@ -1,9 +1,16 @@
 package com.ryan.bookstore.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
 
 @Configuration
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
@@ -11,6 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     /**
      * Frontend CORS 오류로 인한 추가
+     *
      * @param registry
      */
     @Override
@@ -21,5 +29,30 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource : new ClassPathResource("/static/index.html");
+                    }
+                });
+    }
+
+    /**
+     * API 경로가 아닌 모든 요청을 Vue의 index.html로 매핑
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{x:[\\w\\-]+}").setViewName("forward:/");
+        registry.addViewController("/{x:^(?!api$).*}/{y:[\\w\\-]+}").setViewName("forward:/");
+        registry.addViewController("/{x:^(?!api$).*}/{y:[\\w\\-]+}/{z:[\\w\\-]+}").setViewName("forward:/");
     }
 }
