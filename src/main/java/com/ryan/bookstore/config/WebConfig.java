6e95179a9b6249e466418fe66b6ed958de85b24f
Ryan <shariest@naver.com>
2025-03-21 15:01:32 +0900

    [BUG] Book 생성 및 수정 시 Validation 체크 기능 누락 (#8)
    * feature: #6 Frontend API 호출 시 CORS 오류발생 수정
    
    * feature: #6 Backend Book Validation 체크 기능 추가

--- src/main/java/com/ryan/bookstore/config/WebConfig.java
+++ src/main/java/com/ryan/bookstore/config/WebConfig.java
@@ -2,8 +2,24 @@ package com.ryan.bookstore.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * Frontend CORS 오류로 인한 추가
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 }
